a3c8518c8e47ad28fb03356b9a4ff166b4383ac2
yash-455 <yashpatel62495@gmail.com>
2025-09-26 17:00:27 +0530

    assignment_1

---
+++ task_1.py
@@ -0,0 +1,7 @@
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+
+print('Addition = ', a + b)
+print('Subtraction = ', a - b)
+print('Multiplication = ', a * b)
+print('Division = ', a / b)
---
+++ task_2.py
@@ -0,0 +1,6 @@
+f_name = str(input('Enter first name: '))
+l_name = str(input('Enter last name: '))
+
+name = f_name + l_name
+
+print("hello," + name + "! welcome to the python program.")
